a844acb61bbccf7e3511b15fe35883ee809d9ad4
Denis Perepelitsyn <fropsas@gmail.com>
2021-06-16 13:16:52 +0300

    fixed build

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
 import typescript from "@rollup/plugin-typescript";
 import url from "@rollup/plugin-url";
+import pkg from "./package.json";
 
 export default {
   input: "src/index.ts",
   output: [
-    { file: 'dist/index.js', format: "cjs" },
-    { file: 'dist/index2.js', format: "es" },
+    { file: pkg.main, format: "cjs" },
+    { file: pkg.module, format: "es" },
   ],
   plugins: [url(), typescript()],
 };
