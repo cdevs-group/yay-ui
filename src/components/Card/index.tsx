cc584b169ab6a36dafa7ed028e10238c61edb76e
OlgaDorofeeva <vulpecula00@gmail.com>
2021-06-23 17:57:58 +0700

    fix

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,6 @@
 export { default as Card } from "./Card";
 export { default as ExpiredCard } from "./ExpiredCard";
+export { default as LiveCard } from "./LiveCard";
+export { default as LoaderCard } from "./Loader";
 
 export type { CardProps, CardHeaderProps, CardTheme } from "./types";
