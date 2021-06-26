f1c47aaa4499fecd21adf18bcee84325d72937be
Anatoliy <anatoliy@MacBook-Pro-Anatoliy.local>
2021-06-26 17:52:51 +0300

    fixed loader position

--- src/components/CardBTC/index.stories.tsx
+++ src/components/CardBTC/index.stories.tsx
@@ -3,6 +3,9 @@ import ExpiredCardBTC from "./ExpiredCardBTC";
 import CardBTC from "./components/CardBTC";
 import LiveCardBTC from "./LiveCardBTC";
 import GhostCard from "../Card/GhostCard/GhostCard";
+import Loader from "../Card/Loader";
+import Card from "../Card/Card";
+import { CercleIcon } from "../Svg";
 
 export default {
   title: "Components/CardBTC",
@@ -140,6 +143,45 @@ export const CancelCardBTC: React.FC = () => {
       >
         <GhostCard href="#" />
       </CardBTC>
+    </div>
+  );
+};
+
+export const LaterCardBTC: React.FC = () => {
+  return (
+    <div style={{ padding: "32px", width: "500px" }}>
+      <Card
+        live
+        colorNone
+        displayNone
+        hide
+        leftContent={<CercleIcon fill="none" spin />}
+        rightContent="#001"
+        payoutUp={1.03}
+        payoutDown={5.03}
+        time={2000}
+      >
+        <Loader textTooltip="This round`s closing transactions has been submitted to the blockchain, and is awaiting confirmation" />
+      </Card>
+    </div>
+  );
+};
+export const LaterCardTextBTC: React.FC = () => {
+  return (
+    <div style={{ padding: "32px", width: "500px" }}>
+      <Card
+        live
+        colorNone
+        displayNone
+        hide
+        leftContent="LATER"
+        rightContent="#001"
+        payoutUp={1.03}
+        payoutDown={5.03}
+        time={2000}
+      >
+        <Loader />
+      </Card>
     </div>
   );
 };
