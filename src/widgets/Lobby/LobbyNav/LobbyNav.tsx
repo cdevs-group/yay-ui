e5e2c753a94983d012aa5d5d675a9f91271ed801
Anatoliy <sergeev.toll@yandex.ru>
2021-09-28 16:28:39 +0300

    feat: add approve function to nav lobby block

--- src/widgets/Lobby/LobbyNav/LobbyNav.tsx
+++ src/widgets/Lobby/LobbyNav/LobbyNav.tsx
@@ -3,32 +3,53 @@ import { LobbyNavProps } from "../types";
 import styled from "styled-components";
 import { Text } from "../../../components/Text";
 import { UNIT, CONTROLLER } from "../../../constants/images";
+import { Button } from "../../../components/Button";
 
-const LobbyNav = ({ texts, handleJoin, handleCreate, imgJoin, imgCreate }: LobbyNavProps) => {
+const LobbyNav = ({ texts, handleJoin, handleCreate, imgJoin, imgCreate, isApprove, handleApprove }: LobbyNavProps) => {
   return (
-    <Wrapper>
-      <ButtonAdd onClick={handleJoin}>
-        <Text fontSize="19px">{texts.join}</Text>
-        <img src={imgJoin || CONTROLLER} alt="avatar" />
-      </ButtonAdd>
-      <ButtonCreate onClick={handleCreate}>
-        <Text fontSize="19px">{texts.create}</Text>
-        <img className="right" src={imgCreate || UNIT} alt="avatar" />
-      </ButtonCreate>
-    </Wrapper>
+    <NavBlock>
+      <Wrapper id="join">
+        <ButtonAdd onClick={handleJoin}>
+          <Text fontSize="19px">{texts.join}</Text>
+          <img src={imgJoin || UNIT} alt="avatar" />
+        </ButtonAdd>
+        <ButtonCreate onClick={handleCreate}>
+          <Text fontSize="19px">{texts.create}</Text>
+          <img className="right" src={imgCreate || CONTROLLER} alt="avatar" />
+        </ButtonCreate>
+      </Wrapper>
+      <ButtonStyle onClick={handleApprove} variant="green" maxWidth="175px" width="100%">
+        {texts.approve}
+      </ButtonStyle>
+      <BlurBlock blur={isApprove} />
+    </NavBlock>
   );
 };
 
 export default LobbyNav;
 
-const Wrapper = styled.div`
+const NavBlock = styled.div`
+  position: relative;
+  max-width: 222px;
+  overflow: hidden;
+  border-radius: 15px;
+  ${({ theme }) => theme.mediaQueries.md} {
+    max-width: 452px;
+  }
+`;
+const Wrapper = styled.div<{ isApprove: boolean }>`
+  position: relative;
+  max-width: 222px;
   margin: 0 auto;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+  ${({ theme }) => theme.mediaQueries.md} {
+    max-width: 452px;
+  }
 `;
 const ButtonAdd = styled.button`
-  margin: 0 12px 16px;
+  margin: 0 0 16px;
   max-width: 220px;
   min-height: 220px;
   width: 100%;
@@ -46,12 +67,38 @@ const ButtonAdd = styled.button`
     bottom: 0;
     left: 0;
   }
+  ${({ theme }) => theme.mediaQueries.md} {
+    margin: 0 12px 0 0;
+  }
 `;
 
 const ButtonCreate = styled(ButtonAdd)`
+  margin: 0 0 16px;
   background: ${({ theme }) => theme.colors.green};
   & img {
     left: auto;
     right: 0;
   }
+  ${({ theme }) => theme.mediaQueries.md} {
+    margin: 0;
+  }
+`;
+const ButtonStyle = styled(Button)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+`;
+const BlurBlock = styled.div<{ blur: boolean }>`
+  top: 0;
+  backdrop-filter: ${({ blur }) => (blur ? "blur(20px)" : "none")};
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-radius: 15px;
+  position: absolute;
+  background-image: ${`-moz-element(#join)`};
+  -webkit-filter: blur(20px);
+  filter: ${({ blur }) => (blur ? "blur(20px)" : "none")};
 `;
