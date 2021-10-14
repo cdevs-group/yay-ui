69cc1309405026fecb6580c488f68e6ffa7c5b72
Anatoliy <sergeev.toll@yandex.ru>
2021-10-14 20:44:47 +0300

    feat: added-button-winner-new

---
+++ src/components/Button/WinButton.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button } from "./index";
+import { Flex } from "../Box";
+import styled from "styled-components";
+
+const WinButton = ({ children, image, onClick }: { children: string; onClick: () => void; image: string }) => {
+  return (
+    <Flex width="fit-content" position="relative" alignItems="center">
+      <Button onClick={onClick} height="46px" width="100%" maxWidth="198px" padding="0 75px 0 20px" variant="green">
+        {children}
+      </Button>
+      <ImgBlock>
+        <img src={image} />
+      </ImgBlock>
+    </Flex>
+  );
+};
+
+export default WinButton;
+
+const ImgBlock = styled.div`
+  position: absolute;
+  top: -27px;
+  right: -7px;
+  pointer-events: none;
+  cursor: pointer;
+`;
