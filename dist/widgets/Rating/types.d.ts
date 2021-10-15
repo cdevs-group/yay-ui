bc34ab4f95e5d84d3a5e9c207d1ac8258183f744
OlgaDorofeeva <vulpecula00@gmail.com>
2021-10-16 01:32:16 +0700

    build: new dist

--- dist/widgets/Rating/types.d.ts
+++ dist/widgets/Rating/types.d.ts
@@ -24,9 +24,10 @@ export interface PlayerListProps {
     playersList: Array<{
         position?: string | number;
         token?: any;
-        name: string;
+        account: string;
         score?: string;
     }>;
+    tokenIcon?: string;
 }
 export interface ListWrapProps {
     children: ReactNode;
