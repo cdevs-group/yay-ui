992842dc868f028aed80f7ba5a1a40c2d95cf205
OlgaDorofeeva <vulpecula00@gmail.com>
2021-10-16 01:42:51 +0700

    fix: style

--- src/helpers/ellipsis.ts
+++ src/helpers/ellipsis.ts
@@ -1,5 +1,5 @@
 export const ellipsis = (value: string, count = 4) => {
-  if (value.length > 10) {
+  if (value && value.length && value.length > 10) {
     return `${value.substr(0, count)} ... ${value.substr(value.length - count, value.length)}`;
   }
   return value;
