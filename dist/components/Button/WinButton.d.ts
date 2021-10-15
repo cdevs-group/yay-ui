be1b64554e86c03bad0d1b028b33cc9e646aa3d9
Anatoliy <sergeev.toll@yandex.ru>
2021-10-15 11:02:44 +0300

    fix: send dist

---
+++ dist/components/Button/WinButton.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+declare const WinButton: ({ children, image, onClick }: {
+    children: string;
+    onClick: () => void;
+    image: string;
+}) => JSX.Element;
+export default WinButton;
