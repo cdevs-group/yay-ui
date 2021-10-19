1c8b3031f2def1b3d0af8a624fbdfe138bd92f6e
OlgaDorofeeva <vulpecula00@gmail.com>
2021-10-19 21:46:01 +0700

    feat: add profile card

--- src/widgets/Profile/index.stories.tsx
+++ src/widgets/Profile/index.stories.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import ProfileCard from "./ProfileCard";
+import coins from "./images/coins.png";
+import rocket from "./images/rocket.png";
+import skull from "./images/skull.png";
+import win from "./images/win.png";
 
 export default {
   title: "Widgets/Profile",
   argTypes: {},
 };
 
+const textsCard = {
+  title: "User Name",
+  coins: "Coins",
+  games: "games",
+  wins: "wins",
+  losses: "losses",
+  edit: "Edit Profile",
+};
+
 export const ProfileCardWidget: React.FC = () => {
   return (
     <div style={{ marginTop: 50 }}>
-      <ProfileCard />
+      <ProfileCard
+        link="dd"
+        texts={textsCard}
+        account="6x3438e324938438e32438e329828x46R65"
+        awards={[coins, win, skull, rocket]}
+      />
     </div>
   );
 };
