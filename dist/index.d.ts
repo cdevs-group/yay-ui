74741c936412025acb89d4b6e3ea6d6a76432145
OlgaDorofeeva <vulpecula00@gmail.com>
2021-10-19 21:54:09 +0700

    build: new dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -53,5 +53,6 @@ export * from "./widgets/Lobby";
 export * from "./widgets/ModalPlatform";
 export * from "./widgets/GameWidget";
 export * from "./widgets/BridgeWidgetStaking";
+export * from "./widgets/Profile";
 export { default as ResetCSS } from "./ResetCSS";
 export * from "./theme";
---
+++ dist/widgets/Profile/ProfileCard.d.ts
@@ -0,0 +1,4 @@
+import React from "react";
+import { ProfileCardProps } from "./types";
+declare const ProfileCard: React.FC<ProfileCardProps>;
+export default ProfileCard;
