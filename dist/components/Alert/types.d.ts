8e16a1b3993feb76b0fe813aa2f9ce000b33c860
OlgaDorofeeva <vulpecula00@gmail.com>
2021-10-26 22:30:54 +0700

    build: new dist

--- dist/components/Alert/types.d.ts
+++ dist/components/Alert/types.d.ts
@@ -1,5 +1,5 @@
 import { MouseEvent, ReactNode } from "react";
-import { SpaceProps, BackgroundProps } from "styled-system";
+import { SpaceProps, BackgroundProps, LayoutProps } from "styled-system";
 export declare type AlertTheme = {
     background: string;
 };
@@ -11,7 +11,7 @@ export declare const variants: {
     readonly ERROR: "error";
 };
 export declare type Variants = typeof variants[keyof typeof variants];
-export interface AlertWrapperProps extends SpaceProps, BackgroundProps {
+export interface AlertWrapperProps extends SpaceProps, BackgroundProps, LayoutProps {
 }
 export interface AlertProps {
     variant?: Variants;
