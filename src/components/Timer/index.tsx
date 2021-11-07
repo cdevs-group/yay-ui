3966eb6cfb7723744218d70b21a46b04a09368f7
OlgaDorofeeva <vulpecula00@gmail.com>
2021-11-07 21:22:03 +0700

    fix: export

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,3 +1,4 @@
 export { default as Timer } from "./Timer";
 export { default as TimerNotSolid } from "./TimerNotSolid";
 export { default as TimerSimple } from "./TimerSimple";
+export { default as TimerNotSolidWithoutBg } from "./TimerNotSolidWithoutBg";
