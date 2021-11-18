15941694ae00769c7990424e19729f3b09570741
Anatoliy <sergeev.toll@yandex.ru>
2021-11-18 19:30:05 +0300

    feat: created card deatils igo 2

--- src/widgets/IGOPlatformV2/types.ts
+++ src/widgets/IGOPlatformV2/types.ts
@@ -1,3 +1,5 @@
+import { ButtonProps } from "../../components/Button";
+
 export enum StatusType {
   BEFORE_SALE = "BeforeSale",
   SALE = "SALE",
@@ -38,5 +40,62 @@ export interface IGOCardV2Props {
   isStaked?: boolean;
   tperiodText: string;
   inPool: boolean;
-  isClaimed: boolean;
+  propsButtonStake?: ButtonProps;
+  disabledButtonStake?: boolean;
+}
+interface IGOCardDetailText {
+  available: string;
+  availableNetwork: string;
+  button: string;
+  timer: string;
+  copied: string;
+}
+
+export interface IGODetailCardV2Props {
+  site: string;
+  token: string;
+  statusTitle: string;
+  texts: IGOCardDetailText;
+  tokenImg: string;
+  address: string;
+  balance: string;
+  externalLink: string;
+  width: number;
+  networksTab: { value: string | number; img: string }[];
+  handleTab: (e: any) => void;
+  currentNetwork: string | number;
+  tperiodText: string;
+  time: number;
+  linkView: string;
+}
+interface IGOCardDetailProgressText {
+  title: string;
+  stakeButton: string;
+  tier: string;
+  chance: string;
+  allocation: string;
+  summary: string;
+  buttonBuy: string;
+  buttonClaim: string;
+}
+export interface IGODetailProgressCard2Props {
+  texts: IGOCardDetailProgressText;
+  status: StatusType;
+  textBaseProgress: string;
+  baseProgress: number;
+  isStaked: boolean;
+  tierStatus: string;
+  handleStake: () => void | Promise<void>;
+  chance?: string | number;
+  allocation?: string;
+  inPool: boolean;
+  summary: string;
+  disabledClaim?: boolean;
+  disabledBuy?: boolean;
+  handleBuy: () => void | Promise<void>;
+  handleClaim: () => void | Promise<void>;
+  disabledButtonStake?: boolean;
+  propsButtonStake?: ButtonProps;
+  propsButtonBuy?: ButtonProps;
+  propsButtonClaim?: ButtonProps;
 }
