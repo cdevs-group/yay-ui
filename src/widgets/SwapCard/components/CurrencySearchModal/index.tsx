c1001dea2f3b63c499b0bc0cbf6662a458be5c40
OlgaDorofeeva <vulpecula00@gmail.com>
2021-11-23 21:18:21 +0700

    YAY-117 fix exports

--- src/widgets/SwapCard/components/CurrencySearchModal/index.tsx
+++ src/widgets/SwapCard/components/CurrencySearchModal/index.tsx
@@ -1,2 +1,12 @@
 export { default as CurrencySearchModal } from "./CurrencySearchModal";
+export { default as CurrencySearch } from "./CurrencySearch";
+export { default as CurrencyList } from "./CurrencyList";
+export { default as ImportList } from "./ImportList";
+export { default as ImportRow } from "./ImportRow";
+export { default as ImportToken } from "./ImportToken";
+export { default as Manage } from "./Manage";
+export { default as ManageLists } from "./ManageLists";
+export { default as ManageTokens } from "./ManageTokens";
+export { default as CommonBases } from "./CommonBases";
+
 export type { CurrencySearchModalProps } from "./types";
