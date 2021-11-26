db3285726ee0be304f504ab6dee350005c269e75
OlgaDorofeeva <vulpecula00@gmail.com>
2021-11-26 16:09:05 +0700

    YAY-117 fix exports

--- src/components/Layouts/index.tsx
+++ src/components/Layouts/index.tsx
@@ -1,5 +1,7 @@
 export { default as BaseLayout } from "./BaseLayout";
 export { default as Column } from "./Column";
+export { ColumnCenter } from "./Column";
+export { AutoColumn } from "./Column";
 export { default as Container } from "./Container";
 export { RowBetween } from "./Row";
 export { RowFlat } from "./Row";
