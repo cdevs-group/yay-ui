830ff5c1c010f60f214f135235cf64129cb03be6
Anatoliy <sergeev.toll@yandex.ru>
2021-12-16 17:29:35 +0300

    feat: add injected select

---
+++ dist/components/Select/components/Input.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+declare const Input: ({ value, onChange, name, margin }: {
+    margin?: string | undefined;
+    value: string | number;
+    onChange: (e: any) => void;
+    name: string;
+}) => JSX.Element;
+export default Input;
