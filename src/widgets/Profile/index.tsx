d89bcffb8311f4c56bf57cc345f49f09f0d0465e
OlgaDorofeeva <vulpecula00@gmail.com>
2022-01-12 20:17:15 +0700

    fix: exports

--- src/widgets/Profile/index.tsx
+++ src/widgets/Profile/index.tsx
@@ -1,3 +1,4 @@
+export { default as RefferalCard } from "./RefferalCard";
 export { default as ProfileCard } from "./ProfileCard";
 export { default as Achievement } from "./Achievement";
 export { default as AccountProfileBlock } from "./AccountProfileBlock";
