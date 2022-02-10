ae60d07d346ca57fc60600c9608d4d9fb104a682
OlgaDorofeeva <vulpecula00@gmail.com>
2022-02-10 22:50:55 +0700

    build: dist

--- dist/components/Layouts/Column.d.ts
+++ dist/components/Layouts/Column.d.ts
@@ -2,6 +2,6 @@ declare const Column: import("styled-components").StyledComponent<"div", import(
 export declare const ColumnCenter: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {}, never>;
 export declare const AutoColumn: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {
     gap?: string | undefined;
-    justify?: "center" | "end" | "start" | "flex-end" | "flex-start" | "stretch" | "space-between" | undefined;
+    justify?: "space-between" | "stretch" | "center" | "end" | "flex-end" | "flex-start" | "start" | undefined;
 }, never>;
 export default Column;
